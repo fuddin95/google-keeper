959a3f19d584e98c5d577afbb93ca0dc919d0f2e
Muhammad Uddin <fahad.uddin@mail.utoronto.ca>
2020-12-30 00:28:27 -0500

    map the data in notes.js into the website dynamically

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import Note from "./Note";
+import Note, {
+    Card
+} from "./Note";
+import noteMaster from "../notes";
 
 
-function App(){
-    return <div>
-        <Header />
-        <Note />
-        <Footer />
-    </div>
-}
+
+
+function App() {
+    return (<div >
+        <Header / >
+
+        {noteMaster.map((noteMaster) =>
+        <Card 
+        key = {noteMaster.key}     
+        head = {noteMaster.title}
+        para = { noteMaster.content}
+        />
+    )};  
+     
+    <Footer / >
+        </div>);
+            }
+
 
 export default App;
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -9,4 +9,12 @@ function Note(){
     );
 }
 
+function Card(props){
+    return <div className="note">
+        <h1>{props.head}</h1>
+        <p>{props.para}</p>
+    </div>
+}
+
 export default Note;
+export {Card};
